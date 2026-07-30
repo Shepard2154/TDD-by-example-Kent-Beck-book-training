ecbc1adc808c38ca7986d8758b824da01b6d3328
Oleg Burnaev <john-shepard-2154@yandex.ru>
2026-07-30 22:25:13 +0300

    refactor: extract calling of testMethod to run function (page 100)

--- test_custom_xunit.py
+++ test_custom_xunit.py
@@ -4,5 +4,5 @@ from custom_xunit import WasRun
 def test_simple():
     test = WasRun("testMethod")
     assert test.wasRun is None
-    test.testMethod()
-    assert test.wasRun == 1 
+    test.run()
+    assert test.wasRun == 1
